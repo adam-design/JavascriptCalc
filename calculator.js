ee978cfd1680763565af718f75a01353fb6cf462
adam-design <abbridge93@gmail.com>
2015-10-16 22:10:58 -0400

    KeyListener Added
    Now when you press the “enter” key it acts as the =

--- calculator.js
+++ calculator.js
@@ -1,27 +1,15 @@
-// Get all the keys from document
-/*var keys = document.querySelectorAll('#calculator span');
-document.getElementById("#calculator span").innerHTML = "value to display";
-var operators = ['+', '-', 'x', '÷'];
-var decimalAdded = false;
-(function () {
-    'use strict';
-    var i;
-    for (i = 0; i < keys.length; i += 1) {
-        keys[i].onclick = function (e) {
-		// Get the input and button values
-		    var input = document.querySelector('.screen');
-		    var inputVal = input.innerHTML;
-            var btnVal = this.innerHTML;
-		
-		// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
-		// If clear key is pressed, erase everything
-		    if (btnVal === 'C') {
-			    input.innerHTML = '';
-			    decimalAdded = false;
-            }
-		  
-            document.write(inputVal)
-        }; ());
-        */
+        function SimpleStuff()
+        {
+document.getElementById("a").value =            eval(document.getElementById("a").value)
+        }
+        
+      
+         function calcMultiply(button)
+        {
+            document.getElementById("a").value += button.value;
+            document.getElementById("a").focus();
+            
+            
+        }
 
-document.getElementById("screen").innerHTML = "15";
+
